31a0e2896152593376b04a7541b54e0d9e2a464a
makksesh <maksimka2388@gmail.com>
2024-05-27 10:14:47 +0300

    add fill

--- задание 6.cpp
+++ задание 6.cpp
@@ -9,7 +9,9 @@ int main()
 	int *a = new int[5];
 	for (int i = 0; i < 5; i++)
 	{
-		std::cout << "Введите число в ячейку " << i + 1;
+		std::cout << "Введите число в ячейку " << i + 1 << ": ";
+		std::cin >> a[i];
+
 	}
 }
 
